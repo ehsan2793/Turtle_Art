bd9ea6cd6b2f1b86d820170f6c4cd4769f483412
ehsan2793 <ehsan2793@gmail.com>
2022-02-21 16:27:52 -0800

    arts completed successfully

--- main.py
+++ main.py
@@ -10,24 +10,26 @@ for color in colors:
     g = color.rgb.b
     b = color.rgb.b
     rgb_colors.append((r, g, b))
-
+def random_color():
+    r = int(random.randint(40,255))
+    g = int(random.randint(40, 255))
+    b = int(random.randint(30, 255))
+    return  r,g,b
 screen = Screen()
 screen.colormode(255)
 jimmy = Turtle()
 n = 1
 y = 30
+# screen.bgcolor('black')
 jimmy.penup()
-jimmy.setposition(-100.00,-200)
-jimmy.pendown()
-while n < 10:
-    for i in range(5):
-        jimmy.dot(20,random.choice(rgb_colors))
-        jimmy.penup()
+jimmy.setposition(-250.00,-200)
+jimmy.speed(100)
+jimmy.hideturtle()
+while n < 16:
+    for i in range(10):
+        jimmy.dot(20,random_color())
         jimmy.forward(50)
-        jimmy.pendown()
-        jimmy.dot(20, random.choice(rgb_colors))
-    jimmy.penup()
-    jimmy.setposition(-100.00, -200 + y)
+    jimmy.setposition(-250.00, -200 + y)
     n += 1
     y += 30
 
